5405d259ba0346b0ba69a5205429ffee16cfef52
William Lawecki <117457291+Lucky24seven@users.noreply.github.com>
2025-01-22 13:48:48 -0500

    Add files via upload

---
+++ homework1.el
@@ -0,0 +1,21 @@
+; William Lawecki
+; Programming Languages C311
+; 1/20/2025
+
+(* (log 15.0) (+ (* 18 2) 3))
+105.6139578429862
+
+(max 11 37 21 15 8)
+37
+
+(>= (* 3 3.14) (/ 15 2))
+t
+
+'(dogwood elm pine oak ash)
+(dogwood elm pine oak ash)
+
+(car '(dogwood elm pine oak ash))
+dogwood
+
+(message "There are %d minutes in a day." (* 24 60))
+"There are 1440 minutes in a day."
